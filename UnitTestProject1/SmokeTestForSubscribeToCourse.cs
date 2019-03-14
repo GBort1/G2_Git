62a7210b5a1d1a95c8dabf4823f765f1d334a51b
fancy88@ukr.net <fancy88@ukr.net>
2019-03-15 01:43:53 +0200

    Added all steps for the smoke text

--- UnitTestProject1/SmokeTestForSubscribeToCourse.cs
+++ UnitTestProject1/SmokeTestForSubscribeToCourse.cs
@@ -1,9 +1,7 @@
 ﻿using System;
-using System.Collections.Generic;
 using NUnit.Framework;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
-using OpenQA.Selenium.Support.UI;
 
 namespace AutomationTest
 {
@@ -11,7 +9,6 @@ namespace AutomationTest
     public class SmokeTestForSubscribeToCourse
     {
         IWebDriver driver;
-        IWebElement element;
    
 
         [SetUp]
@@ -20,19 +17,16 @@ namespace AutomationTest
             driver = new ChromeDriver();
             driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(10);
             driver.Navigate().GoToUrl("http://old.qalight.com.ua/zapisatsya-na-kursy/");
-          
-       
-                //driver.Navigate().Refresh();
         }
 
         [TearDown]
         public void TearDown()
-        {
+        {   
             driver.Quit();
         }
 
 
-
+        [Category("Smoke")]
         [Test]
         public void TestMethod1()
         {
@@ -40,40 +34,36 @@ namespace AutomationTest
             IWebElement surname = driver.FindElement(By.Id("z_sender0"));
             surname.SendKeys("Surname");
 
+
             IWebElement name = driver.FindElement(By.Id("z_text1"));
             name.SendKeys("Name");
+
 
             IWebElement phone = driver.FindElement(By.Id("z_text0"));
             phone.SendKeys("0111111111");
 
+
             IWebElement email = driver.FindElement(By.Id("z_sender1"));
             email.SendKeys("test@test.com");
+
 
             IWebElement skype = driver.FindElement(By.Id("z_text2"));
             skype.SendKeys("test");
 
-            IWebElement sourceDropDown = driver.FindElement(By.CssSelector("[name='_e926ba2b2813f56de8fc13877057e908']"));
+
+            IWebElement sourceDropDown = driver.FindElement(By.XPath("*//option[@value= 'интернет (поисковая система)']"));
             sourceDropDown.Click();
 
-            IWebElement sendButton = driver.FindElement(By.CssSelector("[type='submit']"));
 
+            IWebElement sendButton = driver.FindElement(By.CssSelector("[type='submit']"));
             sendButton.Click();
 
-     
+            IWebElement alert = driver.FindElement(By.CssSelector("div> .alert-success"));
 
 
-            //var windowHandle = driver.CurrentWindowHandle;
-
-            ////IWebElement element = driver.FindElement(By.CssSelector("test css"));
-
-            ////var elements = driver.FindElements(By.CssSelector("test multi css"));
-
-
-
-
+            Assert.That(alert, Is.Not.Null);
 
         }
-
 
     }
 }
